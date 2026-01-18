eba6aa508c5c9d868d198611bfb410edc3adebc2
Andrew Wang <awwang3@uci.edu>
2026-01-18 08:36:00 -0800

    chore: add next

--- packages/eslint-config/eslint.config.next.js
+++ packages/eslint-config/eslint.config.next.js
@@ -1,18 +1,11 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 import reactConfig from "./eslint.config.react.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const config = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextVitals,
+  ...nextTs,
   ...reactConfig,
   {
     ignores: [
