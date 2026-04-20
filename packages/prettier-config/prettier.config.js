96da1d8914785d37c454eff2040eb1359bb2e579
Andrew Wang <awwang3@uci.edu>
2026-04-20 11:36:14 -0700

    chore: update deps

--- packages/prettier-config/prettier.config.js
+++ packages/prettier-config/prettier.config.js
@@ -7,21 +7,9 @@ const config = {
   tabWidth: 2,
   printWidth: 100,
   plugins: [
-    require.resolve("prettier-plugin-embed"),
     require.resolve("prettier-plugin-packagejson"),
-    require.resolve("prettier-plugin-sh"),
-    require.resolve("prettier-plugin-sql"),
     require.resolve("prettier-plugin-tailwindcss"),
   ],
-
-  // embed plugin
-  embeddedSqlTags: ["sql"],
-
-  // sql plugin
-  language: "postgresql",
-  keywordCase: "upper",
-  dataTypeCase: "upper",
-  functionCase: "upper",
 
   // tailwindcss plugin
   tailwindFunctions: ["cva", "clsx", "cn"],
